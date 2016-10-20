258b8c2a61279196fb02ca8937699a8f4c1f53af
Andrew Koroluk <koroluka@gmail.com>
2016-10-20 08:14:50 -0400

    refactor(demo): move to examples folder, cli-demo

--- examples/cli-demo/demo.js
+++ examples/cli-demo/demo.js
@@ -1,14 +1,16 @@
-import RPLidar from '../src/rplidar';
+import RPLidar from '../../src/rplidar';
 
 let lidar = new RPLidar();
+
+lidar.on('data', console.log);
+
 lidar.init().then(async () => {
     let health = await lidar.getHealth();
     console.log('health: ', health);
 
-
     let info = await lidar.getInfo();
     console.log('info: ', info);
 
-    await lidar.scan();
-    lidar.on('data', console.log);
+    // await lidar.scan();
+    lidar.reset();
 });
